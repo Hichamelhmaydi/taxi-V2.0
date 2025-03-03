3a07ddf16f1fb19d41ff32e927e11862999d5409
Hichamelhmaydi <ct1594065@gmail.com>
2025-03-03 14:51:06 +0000

    creation des migrations

---
+++ database/migrations/2025_03_03_144613_create_avis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avis', function (Blueprint $table) {
+            $table->unsignedBigInteger('chauffeur_id'); 
+            $table->foreign('chauffeur_id')->references('id')->on('users')->onDelete('cascade'); 
+            $table->text('commentaire');
+            $table->integer('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avis');
+    }
+};
---
+++ database/migrations/2025_03_03_144713_create_reservations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id(); 
+            $table->foreignId('passager_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('chauffeur_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('date_heure_depart');
+            $table->string('lieu_depart');
+            $table->string('lieu_arrivee');
+            $table->enum('statut', ['en_attente', 'confirmée', 'annulée', 'terminée'])->default('en_attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
